64169bf27a42f7ba7297fdf654d6f0065c19d6c2
Mike <mallisiscool@gmail.com>
2021-10-15 14:43:26 +0000

    cool swag parser

--- parser.py
+++ parser.py
@@ -3,4 +3,14 @@ import tokens;
 def parse(text):
 	tokenSet = [];
 	allTokens = tokens.allTokens;
+
+	parsingText = text;
+	while parsingText != "":
+		for i in range(0, len(allTokens)):
+			token = allTokens[i];
+			tkStr = token.getText();
+			if parsingText[0:len(tkStr)] == tkStr:
+				tokenSet.append(token);
+				parsingText = parsingText[len(tkStr):];
+	
 	return tokenSet;
